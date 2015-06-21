f3ea6b682349b3785dc65197e41797c9082f0046
Noitidart <noitidart@gmail.com>
2015-06-20 18:58:58 -0700

    2.0a1pre.rev2
    Got the Chrome styling for the most part

--- resources/scripts/cp.js
+++ resources/scripts/cp.js
@@ -41,3 +41,67 @@ window.addEventListener('keyup', function(e) {
 		aContentWindow.dispatchEvent(event);
 	}
 }, false);
+
+
+        var selectDivFullHeights = {};
+        var selectProps = {'customAppButton':1, 'customFxChromeStyle':1};
+        
+        function show(el, state) {
+            el.style.display = !state ? '' : state;
+        }
+        function hide(el) {
+            el.style.display = 'none';
+        }
+        function $(el) {
+            return document.getElementById(el);
+        }
+        function toggleSelect(noAnim, prop) {
+            //prop is string of the pref like ie: customAppButton or customFxChromeStyle
+            //detereminds if customAppButton checkbox is selected, if so shows and hides the select box
+            var checkbox = $(prop);
+            var selectDiv = $(prop + 'SelectDiv');
+            var select = $(prop + 'Select');
+            /*
+            if (!selectDivFullHeights[prop]) {
+                selectDivFullHeights[prop] = window.getComputedStyle(selectDiv, null).getPropertyHeight();
+                selectDiv.style.height = 0;
+            }
+            */
+            if (checkbox.checked) {
+                //show(selectDiv);
+                if (!noAnim) {
+                    show(selectDiv);
+                } else {
+                    //selectDiv.style.height = selectDivFullHeights[prop]+'px';
+                    show(selectDiv);
+                }
+            } else {
+                //alert('do css3anim and afterFinish hide el');
+                hide(selectDiv);
+            }
+        }
+        
+        function messageEsc(e) {
+            if (e.keyCode == 27) {
+                //close the options panel
+                $('Options_Cancel').click();
+                e.returnValue = false;
+                e.stopPropagation();
+                e.preventDefault();
+                return false;
+            }
+        }
+        
+        function init(){
+            for (var p in selectProps) {
+                $(p).addEventListener('change', callback(p), false);
+            }
+            window.addEventListener('keypress', messageEsc, false);
+        }
+        window.addEventListener('DOMContentLoaded', init, false)
+        
+        function callback(id) {
+            return function() {
+                toggleSelect(null, id)
+            }
+        }
